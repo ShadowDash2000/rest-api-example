83efe8fb9cef51a450d398fde472db3451d63a3b
Miller Roman Olegovich <miller.roman@4px.ru>
2024-12-11 13:46:45 +0300

    refactor: added example element in db migration

--- internal/db/postgresql/migrations/20241209180400_song_library.sql
+++ internal/db/postgresql/migrations/20241209180400_song_library.sql
@@ -17,6 +17,15 @@ ALTER TABLE song_library
 CREATE INDEX idx_song_library_group ON song_library ("group");
 CREATE INDEX idx_song_library_song ON song_library (song);
 CREATE INDEX idx_song_library_text ON song_library USING GIN (to_tsvector('simple', text));
+
+INSERT INTO song_library ("group", song, release_date, link, text) VALUES
+(
+ 'Muse',
+ 'Supermassive Black Hole',
+ '16.07.2006',
+ 'https://www.youtube.com/watch?v=Xsp3_a-PMTw',
+ 'Ooh baby, don''t you know I suffer?\nOoh baby, can you hear me moan?\nYou caught me under false pretenses\nHow long before you let me go?\n\nOoh\nYou set my soul alight\nOoh\nYou set my soul alight'
+);
 -- +goose StatementEnd
 
 -- +goose Down
